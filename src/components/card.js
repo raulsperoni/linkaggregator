be637c416b65f82f69ce7f253f31b1c01be724c7
raúl <rsperoni@magnesium.coop>
2021-01-20 17:14:37 -0300

    algo

---
+++ src/components/card.js
@@ -0,0 +1,42 @@
+import PropTypes from "prop-types"
+import React from "react"
+
+const Card = ({ sourceTitle, sourceIcon, title, pubDate, link, featuredImage, contentSnippet }) => {
+
+
+  const fecha = new Date(pubDate)
+  return (
+    <a href={link} className="w-full md:w-2/3 mx-auto my-5 lg:max-w-full lg:flex shadow-lg">
+      <img alt="Placeholder"
+           className="w-full h-32 md:h-48 lg:h-auto lg:w-48 object-cover flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+           src={featuredImage} />
+
+      <div
+        className="border-r border-b border-l border-gray-100 lg:border-l-0 lg:border-t bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+        <div className="mb-4">
+          <div className="text-black font-bold text-xl mb-2">{title}</div>
+          <p className="text-grey-darker text-sm">{contentSnippet}</p>
+        </div>
+        <div className="flex items-center">
+          <span className="text-3xl">{sourceIcon}</span>
+          <div className="text-sm ml-2">
+            <p className="text-black leading-none">publicado en <b>{sourceTitle}</b></p>
+            <p className="text-grey-dark"><small>{fecha.toLocaleDateString()}</small></p>
+          </div>
+        </div>
+      </div>
+    </a>
+  )
+}
+
+  Card.propTypes = {
+    sourceTitle: PropTypes.string.isRequired,
+    sourceIcon: PropTypes.any.isRequired,
+    title: PropTypes.string.isRequired,
+    pubDate: PropTypes.any.isRequired,
+    link: PropTypes.string.isRequired,
+    featuredImage: PropTypes.string.isRequired,
+    contentSnippet: PropTypes.string.isRequired
+  }
+
+  export default Card
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,81 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Card from "../components/card"
+import { SiCodechef, GiFountainPen } from "react-icons/all";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = () => {
+
+  const data = useStaticQuery(graphql`
+    {
+      site {
+        buildTime
+        siteMetadata {
+          author
+          description
+          title
+        }
+      }
+      feedBlogMeta {
+        sourceTitle: title 
+        sourceLink: link
+        sourceDescription: description
+      }
+      allFeedBlog {
+        nodes {
+          title
+          pubDate
+          link
+          featuredImage
+          contentSnippet
+        }
+      }
+      feedRecetarioMeta {
+        sourceTitle: title 
+        sourceLink: link
+        sourceDescription: description 
+      }
+      allFeedRecetario {
+        nodes {
+          title
+          pubDate
+          link
+          featuredImage
+          contentSnippet
+        }
+      }
+   }
+  `)
+
+  const blogPosts = data.allFeedBlog.nodes.map(node => Object.assign(Object.assign(node, data.feedRecetarioMeta),{
+    sourceName:"UnBaúl.com",
+    sourceIcon: <span>✍🏼</span>
+  }))
+  const recetarioPosts = data.allFeedRecetario.nodes.map(node => Object.assign(Object.assign(node, data.feedRecetarioMeta),{
+    sourceName:"Recetario",
+    sourceIcon: <span>👨‍🍳</span>
+  }))
+  const allPosts = blogPosts.concat(recetarioPosts).sort((a, b) => new Date(a.pubDate) < new Date(b.pubDate))
+
+  return (
+
+
+    <Layout>
+      <SEO title="Home" />
+      <div className="flex flex-col">
+      {allPosts.map(node => (
+        <Card title={node.title}
+              contentSnippet={node.contentSnippet}
+              featuredImage={node.featuredImage}
+              link={node.link}
+              pubDate={node.pubDate}
+              sourceTitle={node.sourceName}
+              sourceIcon={node.sourceIcon} />))}
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
